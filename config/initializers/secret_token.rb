5b41ccaade93576d550c33fc9249be5ebeaaffad
novae <novaegr@gmail.com>
2013-05-07 17:38:40 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WedE::Application.config.secret_token = '46d8196901aed93058cfdf704daea56e936d9a2b14f80b974144e92b86515295bf66331a582a9914cef5982f794b19d48f94cf0ac8cd3a8f78b76a63b6e49ed1'
